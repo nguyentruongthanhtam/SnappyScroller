d238338792ae1b8781dbf043d54206ba71bace5e
Tam Nguyen <nguyentruongthanhtam@users.noreply.github.com>
2018-08-29 14:56:54 +0300

    -added indicator click event.

--- snappy-scroller.js
+++ snappy-scroller.js
@@ -1,26 +1,35 @@
 let topArray =  [];
 let pageNumber = 1;
 const indicator = document.querySelector('#indicator');
-
 const container = document.querySelector('.wrapper');
+
 for(let i=0; i<4 ;++i)
 {
   topArray.push(container.children[i]);
   let newElement = document.createElement('span')
+  newElement.addEventListener('click', () => {
+    topArray[i].scrollIntoView({ 
+      behavior: 'smooth' 
+    })
+  })
   newElement.innerHTML =  topArray[i].getAttribute('id');
   indicator.appendChild(newElement);
 }
+indicator.children[0].classList.add('indicator_isActive');
 document.querySelector('#btn').addEventListener('click',()=>{
   if(pageNumber >= 4)
     {
       pageNumber = 0
+      indicator.children[3].classList.remove('indicator_isActive');
+    }
+  else
+    {
+      indicator.children[pageNumber-1].classList.remove('indicator_isActive');
     }
   topArray[pageNumber].scrollIntoView({ 
-  behavior: 'smooth' 
-});
+    behavior: 'smooth' 
+  })
+  indicator.children[pageNumber].classList.add('indicator_isActive');
   pageNumber++
 })
 
-window.addEventListener('scroll',()=>{
-  console.log(window.top)
-})
