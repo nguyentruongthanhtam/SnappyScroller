c0253fca18e4a56173ee4e3581adedad239ea2d2
Tam Nguyen <nguyentruongthanhtam@users.noreply.github.com>
2018-08-30 14:42:18 +0300

    - Added indicator highlight. - Fixed bugs.

--- snappy-scroller.js
+++ snappy-scroller.js
@@ -1,11 +1,14 @@
 let topArray =  [];
 let pageNumber = 1;
-const indicator = document.querySelector('#indicator');
-const container = document.querySelector('.wrapper');
+const pageNumbers = topArray.length;
+let positionArray = [];
+const indicator = document.querySelector('#snappy-scroller-indicator');
+const container = document.querySelector('.snappy-scroller-wrapper');
 
-for(let i=0; i<4 ;++i)
+for(let i=0; i<container.childElementCount ;++i)
 {
   topArray.push(container.children[i]);
+  positionArray.push(container.children[i].offsetTop);
   let newElement = document.createElement('span')
   newElement.addEventListener('click', () => {
     topArray[i].scrollIntoView({ 
@@ -15,21 +18,34 @@ for(let i=0; i<4 ;++i)
   newElement.innerHTML =  topArray[i].getAttribute('id');
   indicator.appendChild(newElement);
 }
-indicator.children[0].classList.add('indicator_isActive');
-document.querySelector('#btn').addEventListener('click',()=>{
-  if(pageNumber >= 4)
-    {
-      pageNumber = 0
-      indicator.children[3].classList.remove('indicator_isActive');
-    }
-  else
-    {
-      indicator.children[pageNumber-1].classList.remove('indicator_isActive');
-    }
-  topArray[pageNumber].scrollIntoView({ 
-    behavior: 'smooth' 
-  })
-  indicator.children[pageNumber].classList.add('indicator_isActive');
-  pageNumber++
+// indicator.children[0].classList.add('indicator_isActive');
+// document.querySelector('#btn').addEventListener('click',()=>{
+//   if(pageNumber >= 4)
+//     {
+//       pageNumber = 0
+//       indicator.children[3].classList.remove('indicator_isActive');
+//     }
+//   else
+//     {
+//       indicator.children[pageNumber-1].classList.remove('indicator_isActive');
+//     }
+//   topArray[pageNumber].scrollIntoView({ 
+//     behavior: 'smooth' 
+//   })
+//   indicator.children[pageNumber].classList.add('indicator_isActive');
+//   pageNumber++
+// })
+
+window.addEventListener('scroll',()=>{
+  let winTop = document.documentElement.scrollTop;
+  let currentPos = Math.floor(winTop / window.innerHeight);
+  toggleIndicatorHighlight('indicator_isActive',currentPos)
 })
 
+toggleIndicatorHighlight= (className,currentPos)=>{
+  indicator.childNodes.forEach((i)=>{
+    if(i.classList)
+      i.classList.remove(className)
+  })
+  indicator.children[currentPos].classList.add(className)
+}
